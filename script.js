10d7d244256da446708b1f9b1bfc8942bb3cbc67
jasilshafi03-cloud <jasilshafi03@gmail.com>
2026-03-08 17:12:13 +0530

    Refactor particle.js initialization and toggle menu

--- script.js
+++ script.js
@@ -18,49 +18,6 @@ move:{enable:true,speed:2}
 
 });
 
-const texts=[
-"Biomedical Engineering Student",
-"AI Healthcare Developer",
-"Medical Device Innovator"
-];
-
-let index=0;
-let charIndex=0;
-let currentText="";
-let isDeleting=false;
-
-function typeEffect(){
-
-const typing=document.querySelector(".typing");
-
-if(!typing)return;
-
-if(!isDeleting && charIndex<=texts[index].length){
-currentText=texts[index].substring(0,charIndex++);
-typing.textContent=currentText;
-}
-
-if(isDeleting && charIndex>=0){
-currentText=texts[index].substring(0,charIndex--);
-typing.textContent=currentText;
-}
-
-if(charIndex===texts[index].length){
-isDeleting=true;
-setTimeout(typeEffect,1000);
-return;
-}
-
-if(isDeleting && charIndex===0){
-isDeleting=false;
-index=(index+1)%texts.length;
-}
-
-setTimeout(typeEffect,isDeleting?50:100);
-}
-
-typeEffect();
-
 const toggle=document.querySelector(".contact-toggle");
 const menu=document.querySelector(".contact-menu");
 
